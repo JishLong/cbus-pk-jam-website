6ab1b14d4f9bee3a600d78a6e08674c03f9d88a7
Josh Long <josh@alstongrove.com>
2025-05-05 22:11:57 -0400

    Add font support and set up full global styling

---
+++ client/src/screens/home/home.component.ts
@@ -0,0 +1,12 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'cpjs-home',
+  templateUrl: 'home.component.html',
+  styleUrl: 'home.component.scss',
+  standalone: true,
+  encapsulation: ViewEncapsulation.None,
+})
+export class HomeComponent {
+
+}
