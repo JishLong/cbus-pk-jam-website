4947e69dcb076e245bf7cb6b7a44d2212379d8e5
Josh Long <102710850+JishLong@users.noreply.github.com>
2025-05-07 23:12:03 -0400

    feat/59: Add responsiveness service (#71)

---
+++ client/src/framework/screen-responsiveness/screen-responsiveness.service.ts
@@ -0,0 +1,42 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { inject, Injectable, RendererFactory2, Signal, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Breakpoint, BREAKPOINT_CLASS_PREFIX, BREAKPOINTS } from './screen-responsiveness.symbols';
+
+@Injectable({ providedIn: 'root' })
+export class ScreenResponsivenessService {
+  private readonly breakpointObserver = inject(BreakpointObserver);
+  private readonly rendererFactory2 = inject(RendererFactory2);
+  private readonly renderer2 = this.rendererFactory2.createRenderer(null, null);
+
+  private readonly activeBreakpoints = signal<Breakpoint[]>([]);
+
+  constructor() {
+    this.breakpointObserver.observe(BREAKPOINTS.map(b => b.breakpoint)).pipe(
+      takeUntilDestroyed(),
+    ).subscribe(result => {
+      const activeBreakpoints: Breakpoint[] = [];
+
+      Object.entries(result.breakpoints).forEach((value, index) => {
+        const breakpoint = BREAKPOINTS[index];
+        const breakpointClassName = BREAKPOINT_CLASS_PREFIX.concat(breakpoint.name);
+        const bodyHasBreakpointClass = document.body.classList.contains(breakpointClassName);
+
+        if (value[1]) {
+          activeBreakpoints.push(breakpoint);
+          if (!bodyHasBreakpointClass) {
+            this.renderer2.addClass(document.body, breakpointClassName);
+          }
+        } else if (bodyHasBreakpointClass) {
+          this.renderer2.removeClass(document.body, breakpointClassName);
+        }
+      })
+
+      this.activeBreakpoints.set(activeBreakpoints);
+    });
+  }
+
+  getActiveBreakpoints(): Signal<Breakpoint[]> {
+    return this.activeBreakpoints;
+  }
+}
